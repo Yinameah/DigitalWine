382974f4c6f35fc4c2a6a8f19c1274d5f25b90b8
Aurélien Cibrario <aurelien.cibrario@gmail.com>
2022-04-08 08:35:15 +0200

    Removed doctype temporarely to manage update

--- digitalwine/digitalwine/doctype/cuvee_operation/cuvee_operation.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Aurélien Cibrario and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Cuvee Operation', {
-	// refresh: function(frm) {
-
-	// }
-});
