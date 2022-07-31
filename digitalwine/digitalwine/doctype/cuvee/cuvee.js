09847b34de11cbfb9e7c9d20e1d81ba900aaa00d
Aurélien Cibrario <aurelien.cibrario@gmail.com>
2022-08-01 00:43:50 +0200

    Cleaned cuvee javascript
    - Only the relevant operation type are now displayed for in and out
    - file prettied

--- digitalwine/digitalwine/doctype/cuvee/cuvee.js
+++ digitalwine/digitalwine/doctype/cuvee/cuvee.js
@@ -1,112 +1,121 @@
 // Copyright (c) 2022, Aurélien Cibrario and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Cuvee', {
+frappe.ui.form.on("Cuvee", {
   //refresh: function(frm) {
   //}
-  setup : function(frm) {
-
-    frm.set_query('other_cuvee', 'ops_in', function(frm) {
+  setup(frm) {
+    frm.set_query("other_cuvee", "ops_in", function (frm) {
       return {
         filters: [
-          ['Cuvee','name','!=',frm.name]
-          // in simple '=' version, can use the simpler version 
+          ["Cuvee", "name", "!=", frm.name],
+          // in simple '=' version, can use the simpler version
           // field : 'value' in a dict
           // {
           //cepage: selected_cepage,
           //millesime: '2021'
           // }
-        ]
-      }
-    })
+        ],
+      };
+    });
   },
 
-  refresh: function(frm) {
+  refresh(frm) {
+    // Add custom button that launch a prompt
     let current_cuvee = frm.docname;
-    frm.add_custom_button('Create Product', () => {
+    frm.add_custom_button("Create Product", () => {
       frappe.prompt(
         [
           {
-            label: 'Bottle',
-            fieldname: 'bottle_capacity',
-            fieldtype: 'Link',
-            options: 'Bottle Capacity',
-            reqd:1
+            label: "Bottle",
+            fieldname: "bottle_capacity",
+            fieldtype: "Link",
+            options: "Bottle Capacity",
+            reqd: 1,
           },
           {
-            label: 'Price',
-            fieldname: 'price',
-            fieldtype: 'Float',
+            label: "Price",
+            fieldname: "price",
+            fieldtype: "Float",
           },
         ],
-        function(args){
-          args['cuvee'] = current_cuvee;
-          frappe.call({
-            method: 'digitalwine.digitalwine.doctype.cuvee.cuvee.create_product',
-            args: args,
-            freeze: true,
-          }).then(r => {
-            // This is not called in case of server error
-            // Here, r.message is what the python function returns
-            console.log(r)
-            frappe.show_alert(
-              "Created product in "+args['bottle_capacity']+" for "+current_cuvee
-            );
-            frappe.set_route('product-wine',r.message)
-          })
+        function (args) {
+          args["cuvee"] = current_cuvee;
+          frappe
+            .call({
+              method:
+                "digitalwine.digitalwine.doctype.cuvee.cuvee.create_product",
+              args: args,
+              freeze: true,
+            })
+            .then((r) => {
+              // This is not called in case of server error
+              // Here, r.message is what the python function returns
+              console.log(r);
+              frappe.show_alert(
+                "Created product in " +
+                  args["bottle_capacity"] +
+                  " for " +
+                  current_cuvee
+              );
+              frappe.set_route("product-wine", r.message);
+            });
         },
-        'Create new product for '+frm.docname,
-        'Create'
-      )
-    })
+        "Create new product for " + frm.docname,
+        "Create"
+      );
+    });
   },
-        //primary_action(values) {
-        //  console.log(values);
-        //  d.hide();
-        //  frappe.call('digitalwine.digitalwine.doctype.cuvee.cuvee.create_product',values);
-        //}
-      //})
-      //d.show();
-    //})
+  //primary_action(values) {
+  //  console.log(values);
+  //  d.hide();
+  //  frappe.call('digitalwine.digitalwine.doctype.cuvee.cuvee.create_product',values);
+  //}
+  //})
+  //d.show();
+  //})
   //},
 });
 
+// Events in the child tables
+// See https://frappeframework.com/docs/v13/user/en/api/form#child-table-events
+frappe.ui.form.on("Cuvee Operation", {
+  // When the row is OPENED as a form
+  form_render(frm, cdt, cdn) {
+    // NOTE : cdt = current document type, cdn = current document name
 
-// FOR REFERENCE : UI Operation on former operations field
-//frappe.ui.form.on('Cuvee Operation', {
-
-//    operations_add(frm, cdt, cdn) { // "links" is the name of the table field in ToDo, "_add" is the event
-//        // frm: current ToDo form
-//        // cdt: child DocType 'Dynamic Link'
-//        // cdn: child docname (something like 'a6dfk76')
-//        // cdt and cdn are useful for identifying which row triggered this event
-//        //frappe.msgprint('You added a Cuvee operation ! 🎉 ');
-//    },
-
-//    form_render(frm,cdt,cdn){
-//        // this gets the row itself, not sure what we can do with this
-//        let row = frappe.get_doc(cdt, cdn);
-
-//    }
-
-//});
-
-frappe.ui.form.on('Cuvee Op In', {
-
-	before_ops_in_remove(frm, cdt, cdn){
-		let row = frappe.get_doc(cdt, cdn);
-		if (row.type === 'Transfer'){
-			//frappe.msgprint('Row rewoved !');
-			//
-		}
-	},
-
-});
-
-frappe.ui.form.on('Cuvee Op Out', {
-
-  ops_out_remove(frm, cdt, cdn){
-      //frappe.msgprint('Row rewoved !');
+    // Could get the operation document like this.
+    // Still confused when the child or the parent is in
+    // charge of the data. But below, for setting "Transfer" as
+    // default value, this approach seems to work ....
+    let row = frappe.get_doc(cdt, cdn);
   },
 
+  // NOTE : This is {fieldname}_add, and is trigger, well, when adding a row
+  ops_in_add(frm, cdt, cdn) {
+    // NOTE : cdt & cdn refer to the row itself
+    frm.set_df_property(
+      "ops_in",
+      "options",
+      ["Harvest", "Transfer", "Buy"],
+      frm.doc.name,
+      "type",
+      cdn
+    );
+  },
+  ops_out_add(frm, cdt, cdn) {
+    frm.set_df_property(
+      "ops_out",
+      "options",
+      ["Transfer", "Sell", "Bottling"],
+      frm.doc.name,
+      "type",
+      cdn
+    );
+
+    // Now set default to transfer, as Harvest is not ok for ops_out
+    let row = frappe.get_doc(cdt, cdn);
+    row.type = "Transfer";
+    frm.refresh_field("ops_out");
+  },
 });
